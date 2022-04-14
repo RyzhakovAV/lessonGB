64482e94d805062d202b0eb207c01ab2c46af74d
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-04-14 14:16:05 +0500

    Example 003 'математические операции'

---
+++ learnCSinGB/lessons/Ex_003_Sum/Program.cs
@@ -0,0 +1,11 @@
+﻿// Example 003 Summa two numbers
+int numberA = 32;
+int numberB = 64;
+double numberC = 17;
+double numberD = 4;
+Console.WriteLine("Cумма двух чисел: " + (numberA + numberB));
+Console.WriteLine("Деление двух чисел (целочисленные): " + (numberB / numberA));
+Console.WriteLine("Деление двух чисел (с точкой): " + (numberC / numberD));
+Console.WriteLine("Остаток от деления двух чисел: " + (numberB % numberA));
+Console.WriteLine("Произведение двух чисел: " + (numberA * numberB));
+Console.WriteLine("Разница двух чисел: " + (numberB - numberA));
