97397100a0af57159bd0dafc02e1edf17f8c1ace
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-04-14 14:47:09 +0500

    Example 005 'my favorite user'

---
+++ learnCSinGB/lessons/Ex_005_if_else/Program.cs
@@ -0,0 +1,8 @@
+﻿// example 005 learn if-else
+Console.WriteLine("Введите ваше имя.");
+string username = Console.ReadLine();
+if (username.ToLower() == "Александр"){
+    Console.WriteLine("Я рад видеть вас Александр!");
+}else{
+    Console.WriteLine("Привет " + username);
+}
