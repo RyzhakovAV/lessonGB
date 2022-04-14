69336adfc1ae86ecbf80b41880b9227963105082
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-04-14 14:28:26 +0500

    Example 004 'математические операции над случайными числами'

---
+++ learnCSinGB/lessons/Ex_004_random_int_number/Program.cs
@@ -0,0 +1,10 @@
+﻿// Example 003 random two numbers
+int numberA = new Random().Next(1,15);
+int numberB = new Random().Next(20,50);
+Console.WriteLine("First number: " + numberA);
+Console.WriteLine("Second number: " + numberB);
+Console.WriteLine("Cумма двух чисел: " + (numberA + numberB));
+Console.WriteLine("Деление двух чисел (целочисленные): " + (numberB / numberA));
+Console.WriteLine("Остаток от деления двух чисел: " + (numberB % numberA));
+Console.WriteLine("Произведение двух чисел: " + (numberA * numberB));
+Console.WriteLine("Разница двух чисел: " + (numberB - numberA));
