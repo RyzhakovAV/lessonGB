441764c43a44209151e7fa04d4cf2c1f24f939c6
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-04-15 11:08:23 +0500

    check pow

---
+++ learnCSinGB/lessons/Ex_009_if_pow/Program.cs
@@ -0,0 +1,11 @@
+﻿// we enter two numbers and check whether the second number is the square of the first
+Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if ((numberA*numberA)==numberB){
+    Console.WriteLine("Второе число является квадратом первого");
+}else{
+    Console.WriteLine("Второе число не является квадратом первого");
+}
+
