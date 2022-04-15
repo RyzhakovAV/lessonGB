43cb1eb7671bb85ee5c86276ded88724bd4680c2
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-04-15 11:42:13 +0500

    day in week. while, switch

---
+++ learnCSinGB/lessons/Ex_010_days/Program.cs
@@ -0,0 +1,31 @@
+﻿// day in week
+Console.WriteLine("Введите день недели от 1 до 7");
+int day = Convert.ToInt32(Console.ReadLine());
+while (day <= 0 || day > 7){
+    Console.WriteLine("Вы ввели некорректное значение. Введите день недели от 1 до 7");
+    day = Convert.ToInt32(Console.ReadLine());
+}
+switch (day){
+    case 1:
+    Console.WriteLine("Понедельник");
+    break;
+    case 2:
+    Console.WriteLine("Вторник");
+    break;
+    case 3:
+    Console.WriteLine("Среда");
+    break;
+    case 4:
+    Console.WriteLine("Четверг");
+    break;
+    case 5:
+    Console.WriteLine("Пятница");
+    break;
+    case 6:
+    Console.WriteLine("Суббота");
+    break;
+    case 7:
+    Console.WriteLine("Воскресенье");
+    break;
+}
+Console.WriteLine(day);
