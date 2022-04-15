6200c83541362423bda084993aea151983a7c666
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-04-15 12:32:23 +0500

    -N...+N

---
+++ learnCSinGB/lessons/Ex_011_numbers/Program.cs
@@ -0,0 +1,9 @@
+﻿// numbers -N...+N
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+int n = N * (-1);
+while (n!=N){
+    Console.Write(n +", ");
+    n++;
+}
+Console.Write(n);
