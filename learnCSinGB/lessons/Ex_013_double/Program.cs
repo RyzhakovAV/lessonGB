3a1f1795193f9e99319b56f8940ad71a67eeb36e
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-04-15 12:55:14 +0500

    double number

---
+++ learnCSinGB/lessons/Ex_013_double/Program.cs
@@ -0,0 +1,8 @@
+﻿//
+Console.WriteLine("Введите число N, число не должно быть равно 0");
+double N = Convert.ToDouble(Console.ReadLine());
+while (N == 0){
+    Console.WriteLine("Ошибка ввода. Введите число N, число не должно быть равно 0");
+    N = Convert.ToDouble(Console.ReadLine());
+}
+Console.WriteLine(1 / N);
