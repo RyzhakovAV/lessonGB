da4f4462653b567395ca59d681d25b041acfe5b7
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-04-16 22:00:27 +0500

    find in array

---
+++ learnCSinGB/lessons/Ex_016_MethodArray_2/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] arrayNumbers = {1, 67, 34, 90, 2, 156, 1234, 67, 198, 156};
+int find = 156;
+int n = arrayNumbers.Length;
+int index = 0;
+
+while (index < n){
+    if (find == arrayNumbers[index]){
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
