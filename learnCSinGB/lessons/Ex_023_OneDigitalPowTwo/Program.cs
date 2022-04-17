1cd600dc2139bb9e1e8d25c0d1ff3b9e0ba8366b
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-04-17 15:11:50 +0500

    на входе два числа, проверяем, является ли второе число квадратом первого

---
+++ learnCSinGB/lessons/Ex_023_OneDigitalPowTwo/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу, которая принимает на
+вход два числа и проверяет, является ли второе
+число квадратом первого.
+5, 25 -> да
+-4, 16 -> да
+25, 5 -> нет
+8,9 -> нет
+*/
+Console.Write("Введите первое число: ");
+int N1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int N2 = Convert.ToInt32(Console.ReadLine());
+if ((N1*N1)==N2){
+    Console.WriteLine("да");
+}else{
+    Console.WriteLine("нет");
+}
