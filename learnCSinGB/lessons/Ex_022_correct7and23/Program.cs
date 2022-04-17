93c83cae9325482051b1f6e7c1a4fcdbf2d769fe
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-04-17 14:52:10 +0500

    number correct 7 and 23

---
+++ learnCSinGB/lessons/Ex_022_correct7and23/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Напишите программу, которая принимает на
+вход число и проверяет, кратно ли оно
+одновременно 7 и 23
+*/
+Console.WriteLine("Введите первое число: ");
+int N1 = Convert.ToInt32(Console.ReadLine());
+while (N1 == 0){
+    Console.WriteLine("Ошибка ввода. Введите первое число, которое не равно 0: ");
+    N1 = Convert.ToInt32(Console.ReadLine());
+}
+if (N1%7 == 0 && N1%23 == 0){
+    Console.WriteLine("Число кратное");
+}else{
+    Console.WriteLine("Число не кратное");
+}
