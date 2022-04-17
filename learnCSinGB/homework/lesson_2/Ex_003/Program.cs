522097a522d188d5880521581c89d4512d13a820
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-04-17 16:17:49 +0500

    homework 2_3

---
+++ learnCSinGB/homework/lesson_2/Ex_003/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Напишите программу, которая принимает на
+вход цифру, обозначающую день недели, и проверяет,
+является ли этот день выходным.
+*/
+Console.WriteLine("Введите число от 1-7: ");
+int day = Convert.ToInt32(Console.ReadLine());
+while (day <= 0 || day > 7){
+    Console.WriteLine("Введите число от 1-7: ");
+    day = Convert.ToInt32(Console.ReadLine());
+}
+if (day == 6 || day == 7){
+    Console.WriteLine("Выходной день");
+}else{
+    Console.WriteLine("Будни");
+}
