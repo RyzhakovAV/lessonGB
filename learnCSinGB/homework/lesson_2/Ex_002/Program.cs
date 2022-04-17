61dcf58804cc38392676a7d91572a376c39bdc45
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-04-17 15:59:06 +0500

    третья цифра в числе

---
+++ learnCSinGB/homework/lesson_2/Ex_002/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Напишите программу, которая выводит
+третью цифру заданного числа или сообщает, что третьей
+цифры нет.
+*/
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N/100 <= 0){
+    Console.WriteLine("Третьей цифры нет");
+}else if(N/1000 > 0 ){
+    while(N/1000 > 0){
+        N/=10;
+    }
+    Console.WriteLine(N%10);
+}else{
+    Console.WriteLine(N%10);
+}
