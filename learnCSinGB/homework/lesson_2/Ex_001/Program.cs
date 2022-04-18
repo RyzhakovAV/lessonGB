dd3ac7173a1688812ce93112672ec5bd172cf505
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-04-18 08:55:02 +0500

    modification file lesson_2/ex_001 исправлено условие на более читаемое

--- learnCSinGB/homework/lesson_2/Ex_001/Program.cs
+++ learnCSinGB/homework/lesson_2/Ex_001/Program.cs
@@ -5,7 +5,7 @@
 */
 Console.Write("Введите трезначное число: ");
 int N = Convert.ToInt32(Console.ReadLine());
-while (N/1000 > 0 || N/100 == 0){
+while (N > 999 || N < 100){
     Console.Write("Вы ввели не корректные данные. Пожалуйста введите трезначное число: ");
     N = Convert.ToInt32(Console.ReadLine());
 }
