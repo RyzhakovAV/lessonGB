91beb307de58a3a3f677b29f8bdfd55b46844621
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-04-18 08:58:04 +0500

    modification file lesson_2/ex_002 исправлено условие на более читаемое и ввод числа по модулю

--- learnCSinGB/homework/lesson_2/Ex_002/Program.cs
+++ learnCSinGB/homework/lesson_2/Ex_002/Program.cs
@@ -5,10 +5,11 @@
 */
 Console.Write("Введите число: ");
 int N = Convert.ToInt32(Console.ReadLine());
-if (N/100 <= 0){
+N = Math.Abs(N);
+if (N < 100){
     Console.WriteLine("Третьей цифры нет");
-}else if(N/1000 > 0 ){
-    while(N/1000 > 0){
+}else if(N > 999){
+    while(N > 999){
         N/=10;
     }
     Console.WriteLine(N%10);
