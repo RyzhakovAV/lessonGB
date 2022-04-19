885ddcfdb1c622bbe2a67b3e1415cba7306bedef
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-04-19 12:33:51 +0500

    new file task about method

---
+++ learnCSinGB/lessons/Ex_025_method_quadrant/Program18.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите квандратн 1-4: ");
+int j = Convert.ToInt32(Console.ReadLine());
+
+while (j <= 0 || j> 4){
+    Console.Write("Ошибка ввода. Введите квандратн 1-4: ");
+    j = Convert.ToInt32(Console.ReadLine());
+}
+
+getStart (j);
+
+void getStart (int number)
+{
+    if (number == 1)
+    Console.WriteLine("В первом квандранте числа могут быть x > 0 и y > 0");
+    if (number == 2)
+    Console.WriteLine("В первом квандранте числа могут быть x < 0 и y > 0");
+    if (number == 3)
+    Console.WriteLine("В первом квандранте числа могут быть x < 0 и y < 0");
+    if (number == 4)
+    Console.WriteLine("В первом квандранте числа могут быть x > 0 и y < 0");
+}
