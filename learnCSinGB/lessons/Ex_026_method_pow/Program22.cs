cbb210d31c39c9800ad912a41d80c800132a5559
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-04-19 13:25:18 +0500

    using method for pow number

---
+++ learnCSinGB/lessons/Ex_026_method_pow/Program22.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N != 0){
+    WriteNumber(N);
+}else{
+    Console.WriteLine("На ноль и ответ " + 0);
+}
+
+void WriteNumber(int number){
+    for (int i = 1; i <= Math.Abs(N); i++){
+        if (N >= 0){
+            Console.WriteLine($"Для числа {i} квадрат {i * i}");
+        }else{
+            if (i % 2 == 0){
+                Console.WriteLine($"Для числа {-i} квадрат  {i * i}");
+            }else{
+                Console.WriteLine($"Для числа {-i} квадрат {-i * i}");
+            }
+        }
+    }
+}
