cd2b1c9c2edc9286ced0cd3f69e51e59bd397b27
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-04-20 12:39:29 +0500

    modification gitignore and add task about palindrom number

---
+++ learnCSinGB/homework/lesson_3/Ex_001/Program19.cs
@@ -0,0 +1,43 @@
+﻿/*
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+23432 -> да
+12821 -> да
+*/
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N==0 || Math.Abs(N) < 10){
+    Console.Write("число является палиндромом");
+}else{
+    Polindrom(N);
+}
+
+void Polindrom (int number){
+    number = Math.Abs(number);
+    int n = number;
+    int a = 0, diff=0;
+    while (n > 0){         //Считаем количество цифр в числе      
+        a++;
+        n/=10;
+    }
+    int[] digits = new int[a];          // создаем массив и заполляем цифрами
+    for (int i = a-1; i >= 0; i--){
+        digits[i] = number % 10;
+        number /= 10;
+    }
+
+    for (int i = 0; i < a/2; i++){
+        if (digits[i] != digits[a-i-1]){
+            diff = 0;
+            break;
+        }else{
+            diff++;
+        }
+    }
+    if (diff != 0){
+        Console.WriteLine("число является палиндромом");
+    }else{
+        Console.WriteLine("число не является палиндромом");
+    }                     
+}
