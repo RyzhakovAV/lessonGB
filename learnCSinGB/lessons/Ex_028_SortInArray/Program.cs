6b4cf5eafcd57570fcb040ac20d6eb12f75e5290
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-04-24 23:19:39 +0500

    Sort array

---
+++ learnCSinGB/lessons/Ex_028_SortInArray/Program.cs
@@ -0,0 +1,26 @@
+﻿// алгоритм сортирока методом выбора (мин - макс)
+int [] arr = {1, 2, 3, 4, 5, 6, 71, 2, 4, 1, 5, 9, 7};
+
+void PrintArray(int[] array){
+    int count = array.Length;
+
+    for (int i=0; i < count; i++){
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void SelectionSort(int[] array){
+    for (int i = 0; i < array.Length; i++){
+        int minPosition = i;
+        for(int j = i+1; j < array.Length; j++ ){
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+SelectionSort(arr);
+PrintArray(arr);
