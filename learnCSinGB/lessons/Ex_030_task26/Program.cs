bde5b9ab5bd1066ad712439c7687a0d88d8f77ee
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-04-25 13:23:58 +0500

    example 030 task about count digits

---
+++ learnCSinGB/lessons/Ex_030_task26/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Напишите программу, которая принимает
+на вход число и выдаёт количество цифр в числе.
+*/
+int number;
+Console.Write("Введите число: ");
+while(!int.TryParse(Console.ReadLine(), out number)) {
+    Console.WriteLine("Ошибка ввода");
+    Console.Write("Введите число: ");
+}
+
+int i = 0;
+number=Math.Abs(number);
+while(number>0){
+    i++;
+    number/=10;
+}
+
+Console.WriteLine("Вы ввели: " + i + " значное число");
