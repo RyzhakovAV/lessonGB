64aa50b5e3e86afb6a07eaca95eda0f9a4c6f581
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-04-26 21:32:36 +0500

    homework task 25

---
+++ learnCSinGB/homework/lesson_4/task_25/Program25.cs
@@ -0,0 +1,26 @@
+﻿/*
+Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+int a, b;
+bool exit = true;
+
+void Input(){
+    Console.WriteLine("Введите число: ");
+    while (!int.TryParse(Console.ReadLine(), out a) )    {
+        Console.WriteLine("Ошибка ввода. Пожалуйста, введите число: ");
+    }
+    Console.WriteLine("Введите cтепень: ");
+    while (!int.TryParse(Console.ReadLine(), out b))    {
+        Console.WriteLine("Ошибка ввода. Пожалуйста, введите степень: ");
+    }
+    Console.WriteLine($"{a} в степени {b} равно {Math.Pow(a, b)}");
+}
+
+while (exit){
+    Input();
+    Console.WriteLine("Для выхода из программы введите exit. Для продолжения нажмите Enter");
+    string str = Console.ReadLine();
+    if (str.Equals("exit")) exit = false;
+}
