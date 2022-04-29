be416e996e16a06323bc7acb332b208ac4438111
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-04-29 13:27:32 +0500

    task 32 about replace digit in array

---
+++ learnCSinGB/lessons/Ex_038_task_32/Program32.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задача 32: Напишите программу замена элементов
+массива: положительные элементы замените на
+соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2]
+*/
+int[] array = new int[6];
+
+void FillArray(int[] arr){
+    for(int i =0; i < arr.Length; i++){
+        arr[i] = new Random().Next(-9, 10);
+    }
+}
+void PrintArray(int[] arr){
+    for(int i = 0; i < arr.Length; i++){
+        if (i==0) Console.Write($"[{arr[i]}, ");
+        else if (i==arr.Length-1) Console.Write($"{arr[i]}]");
+        else Console.Write($"{arr[i]}, ");
+    }
+}
+
+int[] ReplaceArray(int[] arr){
+    for(int i = 0; i < arr.Length; i++){
+        if (arr[i] < 0) arr[i] = Math.Abs(arr[i]);
+        else arr[i] *= -1;
+    }
+    return arr;
+}
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int[] Array = ReplaceArray(array);
+PrintArray(Array);
