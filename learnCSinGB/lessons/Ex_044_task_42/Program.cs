91b51f955ff791b1c46291239607f9267575e323
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-05-02 00:11:51 +0500

    example 044 task 42 about 10 -> 2

---
+++ learnCSinGB/lessons/Ex_044_task_42/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 42: Напишите программу, которая будет преобразовывать
+десятичное число в двоичное.
+45 -> 101101
+3 -> 11
+2 -> 10
+*/
+
+int N, number = 0, count = 1;
+
+Console.Write("Введите число: ");
+while (!int.TryParse(Console.ReadLine(), out N)){
+    Console.WriteLine("Ошибка ввода");
+    Console.WriteLine("Введите число: ");
+}
+int n = N;
+while (N > 0){
+    number += (N%2)*count;
+    N /= 2;
+    count *= 10;
+}
+
+Console.Write($"число {n} в двоичном представлении равно {number}");
