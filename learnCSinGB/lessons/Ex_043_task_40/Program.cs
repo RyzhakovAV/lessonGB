0bc6631bcd3d9b0a75daec22723799c490c71ec9
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-05-01 22:52:39 +0500

    example 43 task 40 проверка о существовании треугольника

---
+++ learnCSinGB/lessons/Ex_043_task_40/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 40: Напишите программу, которая принимает на вход три
+числа и проверяет, может ли существовать треугольник с
+сторонами такой длины.
+Теорема о неравенстве треугольника: каждая сторона треугольника
+меньше суммы двух других сторон.
+*/
+int[] array = new int[3];
+int number;
+
+
+
+for (int i =0; i <=2; i++){
+    Console.Write($"Введите {i+1} число: ");
+    while (!int.TryParse(Console.ReadLine(), out number)){
+            Console.WriteLine($"Ошибка ввода");
+            Console.WriteLine($"Введите {i+1} число: "); 
+    }
+    array[i] = number;
+}
+
+if ((array[0] < array[1]+array[2]) && (array[1] < (array[0]+array[2])) && (array[2] < (array[1]+array[0]))){
+    Console.WriteLine("Такой треугольник существует");
+}else{
+    Console.WriteLine("Такого треугольника не существует");   
+}
