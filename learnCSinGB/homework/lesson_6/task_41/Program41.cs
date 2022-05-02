91767400048054eb504b95b9af8ef0ff4ddee701
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-05-02 12:54:51 +0500

    homework task 41 подсчет вводимых чисел больше 0

---
+++ learnCSinGB/homework/lesson_6/task_41/Program41.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3
+*/
+int stop, N, count = 0;
+string str = "";
+
+int Input(){
+    Console.WriteLine("Введите число, для выхода из программы введите exit: ");
+    str = Console.ReadLine()!;
+    if (str.Equals("exit")){
+        return 666;
+    }else{
+        while(!int.TryParse(str, out N)){
+            Console.WriteLine("Ошибка ввода, введите число или exit: ");
+            str = Console.ReadLine()!;
+            if (str.Equals("exit")){
+                return 666;
+            }
+        }                
+    }
+    N = int.Parse(str); 
+    if (N > 0) return 1;
+    else return 0;
+}
+
+while (true) {
+    stop = Input();
+    if (stop != 666) count += stop;
+    else break;
+}
+Console.WriteLine($"Вы ввели {count} числа которые больше 0");
