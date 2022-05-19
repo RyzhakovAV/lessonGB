ef4cdc114736990486e270a9a03dfd7d8e79020e
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-05-19 08:58:47 +0500

    add task 48 and task 48 from lesson

---
+++ learnCSinGB/lessons/task48/Program48.cs
@@ -0,0 +1,38 @@
+﻿/*
+Решение в группах задачи:
+Задача 48: Задайте двумерный массив размера m на n,
+каждый элемент в массиве находится по формуле: Aₘₙ = m+n.
+Выведите полученный массив на экран.
+*/
+
+int m,n;
+
+Console.Write("Введите количество строк (n): ");
+while(!int.TryParse(Console.ReadLine(), out n ) || n <= 0){
+    Console.Write("Ошибка ввода. Введите количество строк (n), число должно быть больше 0: ");
+}
+Console.Write("Введите количество столбцов (m): ");
+while(!int.TryParse(Console.ReadLine(), out m ) || m <= 0){
+    Console.Write("Ошибка ввода. Введите количество строк (m), число должно быть больше 0: ");
+}
+
+int[,] arr = new int[m,n];
+
+FillArray(arr);
+PrintArray(arr);
+
+void FillArray(int[,] array){
+    for (int i =0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) {
+            array[i,j] = i + j;
+        }
+    }
+}
+
+void PrintArray(int[,] array){
+    for (int i =0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) {
+            Console.WriteLine($"array[{i},{j}] = {array[i,j]}");
+        }
+    }
+}
---
+++ learnCSinGB/lessons/task_46/Program46.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 46: Задайте двумерный массив размером m×n,
+заполненный случайными целыми числами.
+*/
+
+const int m = 4;
+const int n = 3;
+
+int[,] arr = new int[m,n];
+
+FillArray(arr, -100, 100);
+PrintArray(arr);
+
+void FillArray(int[,] array, int min, int max){
+    Random rnd = new Random ();
+    for (int i =0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) {
+            array[i,j] = rnd.Next(min, max);
+        }
+    }
+}
+
+void PrintArray(int[,] array){
+    for (int i =0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) {
+            Console.WriteLine($"array[{i},{j}] = {array[i,j]}");
+        }
+    }
+}
