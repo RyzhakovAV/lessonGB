dc2641d80c71aeda05f7dda09bea17f423e04706
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-05-19 09:02:21 +0500

    modified task 48

--- learnCSinGB/lessons/task48/Program48.cs
+++ learnCSinGB/lessons/task48/Program48.cs
@@ -32,7 +32,8 @@ void FillArray(int[,] array){
 void PrintArray(int[,] array){
     for (int i =0; i < array.GetLength(0); i++) {
         for (int j = 0; j < array.GetLength(1); j++) {
-            Console.WriteLine($"array[{i},{j}] = {array[i,j]}");
+            Console.Write($"array[{i},{j}] = {array[i,j]}  ");
         }
+        Console.WriteLine();
     }
 }
