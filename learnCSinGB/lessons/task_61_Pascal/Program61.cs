1cd36d63983d20740f1f8daa1e4bc700442b4e52
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-05-22 18:01:20 +0500

    pascal

---
+++ learnCSinGB/lessons/task_61_Pascal/Program61.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 61: Вывести первые N строк треугольника
+Паскаля. Сделать вывод в виде равнобедренного
+треугольника
+*/
+
+int n = 5;
+
+int[,] array = new int[n, n];
+for (int i = 0; i < array.GetLength(0); i++)
+    for (int j = 0; j < array.GetLength(1); j++)
+        array[i, j] = 1 + j + i;
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j <= i; j++)
+        Console.Write($"{array[i, j]} \t");
+    Console.WriteLine();
+
+}
