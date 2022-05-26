77c50484ff19e78c111986a5f7323ba5aa7a7b5a
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-05-26 23:19:31 +0500

    recursion 1...n

---
+++ learnCSinGB/lessons/task_63/Program63.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 63: Задайте значение N. Напишите программу, которая выведет
+все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"
+*/
+
+int N;
+
+Console.Write("Введите переменную N: ");
+while (!int.TryParse(Console.ReadLine(), out N) || N <=0) {
+    Console.WriteLine("Ошибка ввода. Введите положительное число N: ");
+}
+
+NaturalDigits(N);
+
+void NaturalDigits (int n) {
+    if (n == 1){
+        Console.Write(n + " ");
+    }else{
+        NaturalDigits(n-1);
+        Console.Write(n + " ");
+    }
+}
