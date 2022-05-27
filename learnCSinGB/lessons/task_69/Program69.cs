9e916fdb14f565f4f22eb15f7430d00881c23732
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-05-27 11:43:17 +0500

    task 69 recursion power digit

---
+++ learnCSinGB/lessons/task_69/Program69.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+возводит число А в целую степень B с помощью рекурсии.
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8
+*/
+
+int A = InputValue ("Input number: ");
+int B = InputValue ("Input number: ");
+
+Console.WriteLine($"Число {A} в степени {B} = {PowerNumber(A, B)}");
+
+int InputValue (string element) {
+    int value;
+    Console.Write($"Please. {element}");
+    while (!int.TryParse(Console.ReadLine(), out value)) {
+        Console.Write($"Wrong input. Please. {element}");
+    }
+    return value;
+}
+
+int PowerNumber (int a, int b) {
+    if (b == 0){
+        return 1;
+    }else if (b == 1){
+        return a;
+    }else{
+        return a * PowerNumber(a, b-1);
+    } 
+}
